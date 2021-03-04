4d2d9d17f00114b7e7c064321c2642297fd24029
ctamaral2018 <78878606+ctamaral2018@users.noreply.github.com>
2021-03-04 12:22:28 +0100

    Código incompleto. 04/03/2021

---
+++ FUTBOLARIOS/src/main/java/com/FUTBOLARIOS/FutbolariosApplication.java
@@ -0,0 +1,13 @@
+package com.FUTBOLARIOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbolariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutbolariosApplication.class, args);
+	}
+
+}
---
+++ FUTBOLARIOS/src/test/java/com/FUTBOLARIOS/FutbolariosApplicationTests.java
@@ -0,0 +1,15 @@
+package com.FUTBOLARIOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*
+@SpringBootTest
+class FutbolariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
